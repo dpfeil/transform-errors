adfa88625bebb9109b7627479040ef79a5ff319b
Doug Pfeil <dpfeil@gmail.com>
2020-08-05 03:31:57 -0400

    Flatten all except specified keys

--- transform-errors.js
+++ transform-errors.js
@@ -1,6 +1,8 @@
 
 function lazyCurry(fn) { return (...args) => fn.bind(null, ...args); };
 const map = lazyCurry((fn, arr) => arr.map(fn));
+function mapWith(...args) { return lazyCurry((fn, arr) => arr.map(fn.bind(null, [...args]))); };
+function pipe (...fns) { return x => fns.reduce((v, f) => f(v), x); };
 
 
 function concat (map) {
@@ -11,11 +13,18 @@ function unique (map) {
   return map.toSet().toList();
 }
 
-function flatten (map) {
-  return concat(unique(map.toList().flatten(false)));
+function flatten (keys, map, key) {
+  if(keys.indexOf(key) !== -1)
+    return map;
+  return map.toList().flatten(false);
 }
 
+function transform (...args) { 
+  return pipe(
+    mapWith(...args)(flatten),
+  );
+};
 
-module.exports = map(flatten);
+module.exports = (errors, ...args) => transform(...args)(errors);
 
 

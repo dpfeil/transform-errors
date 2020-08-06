354d3c74df603e3b2ed40eb6bcf3566f435f1513
Doug Pfeil <dougpfeil@MacBook-Pro.fios-router.home>
2020-08-06 16:07:18 -0400

    Add fn to traverse nested structures (has errors)

--- transform-errors.js
+++ transform-errors.js
@@ -1,30 +1,37 @@
+const { List } = require("immutable");
 
-function lazyCurry(fn) { return (...args) => fn.bind(null, ...args); };
+function lazyCurry(fn) {
+  return (...args) => fn.bind(null, ...args);
+}
 const map = lazyCurry((fn, arr) => arr.map(fn));
-function mapWith(...args) { return lazyCurry((fn, arr) => arr.map(fn.bind(null, [...args]))); };
-function pipe (...fns) { return x => fns.reduce((v, f) => f(v), x); };
+function mapWith(...args) {
+  return lazyCurry((fn, arr) => arr.map(fn.bind(null, [...args])));
+}
+function pipe(...fns) {
+  return (x) => fns.reduce((v, f) => f(v), x);
+}
 
-
-function concat (map) {
+function concat(keys, map) {
   return map.join(". ") + ".";
 }
 
-function unique (map) {
+function unique(keys, map) {
   return map.toSet().toList();
 }
 
-function flatten (keys, map, key) {
-  if(keys.indexOf(key) !== -1)
-    return map;
+function traverse(keys, map) {
+  if (List.isList(map) && map.every((x) => typeof x === "string"))
+    return map.map(unique.bind(null, keys)).map(concat.bind(null, keys));
+  return map.map(traverse.bind(null, keys));
+}
+
+function flatten(keys, map, key) {
+  if (keys.indexOf(key) !== -1) return map;
   return map.toList().flatten(false);
 }
 
-function transform (...args) { 
-  return pipe(
-    mapWith(...args)(flatten),
-  );
-};
+function transform(...args) {
+  return pipe(mapWith(...args)(flatten), mapWith(...args)(traverse));
+}
 
 module.exports = (errors, ...args) => transform(...args)(errors);
-
-
